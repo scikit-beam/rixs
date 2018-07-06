bd0b95f0fac8bf8a66831ffa531a978d5c28d787
awalter <awalter@bnl.gov>
2018-07-06 11:29:17 -0400

    initial commit

---
+++ README.rst
@@ -0,0 +1,20 @@
+===============================
+rixs
+===============================
+
+.. image:: https://img.shields.io/travis/awalter-bnl/rixs.svg
+        :target: https://travis-ci.org/awalter-bnl/rixs
+
+.. image:: https://img.shields.io/pypi/v/rixs.svg
+        :target: https://pypi.python.org/pypi/rixs
+
+
+Analysis tools for resonant inelastic xray scattering.
+
+* Free software: 3-clause BSD license
+* Documentation: (COMING SOON!) https://awalter-bnl.github.io/rixs.
+
+Features
+--------
+
+* TODO
---
+++ setup.py
@@ -0,0 +1,66 @@
+from os import path
+from setuptools import setup, find_packages
+import sys
+import versioneer
+
+
+# NOTE: This file must remain Python 2 compatible for the foreseeable future,
+# to ensure that we error out properly for people with outdated setuptools
+# and/or pip.
+if sys.version_info < (3, 6):
+    error = """
+rixs does not support
+Python 2.x, 3.0, 3.1, 3.2, 3.3, 3.4, or 3.5.
+Python 3.6 and above is required. Check your Python version like so:
+
+python --version
+
+This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
+Upgrade pip like so:
+
+pip install --upgrade pip
+"""
+    sys.exit(error)
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with open(path.join(here, 'requirements.txt')) as requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    requirements = [line for line in requirements_file.read().splitlines()
+                    if not line.startswith('#')]
+
+
+setup(
+    name='rixs',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Analysis tools for resonant inelastic xray scattering.",
+    long_description=readme,
+    author="Brookhaven National Lab",
+    author_email='awalter@bnl.gov',
+    url='https://github.com/awalter-bnl/rixs',
+    packages=find_packages(exclude=['docs', 'tests']),
+    entry_points={
+        'console_scripts': [
+            # 'some.module:some_function',
+            ],
+        },
+    include_package_data=True,
+    package_data={
+        'rixs': [
+            # When adding files here, remember to update MANIFEST.in as well,
+            # or else they will not be included in the distribution on PyPI!
+            # 'path/to/data_file',
+            ]
+        },
+    install_requires=requirements,
+    license="BSD (3-clause)",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
