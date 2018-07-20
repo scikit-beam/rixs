19798564111236ed22bf4c5cf0a07846f346708d
Travis CI User <travis@example.org>
2018-07-20 10:37:45 +0000

    Update docs after building Travis build 23 of scikit-beam/rixs
    The docs were built from the branch 'master' against the commit
    b8dbecdad3958a901d282a49e25c2325657a1811.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/scikit-beam/rixs/jobs/406186075.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post15+gf0aa832',
+    VERSION: '0.post20+gb8dbecd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
