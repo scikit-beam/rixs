5d512fedf58f5e7227ac6ec2e37ce30aa565e6a9
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-01-07 11:56:08 +0000

    Update docs after building Travis build 38 of scikit-beam/rixs
    The docs were built from the branch 'master' against the commit
    4991d5e0205e69e64e5a12d7eae5fc0927ffdadb.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/scikit-beam/rixs/jobs/476322373.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.post10+g3eacbf0',
+    VERSION: '0.0.1.post14+g4991d5e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
